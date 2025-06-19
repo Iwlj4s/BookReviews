6ca76df2f2fdb9654f862db747d6a66c2d53e96a
Iwlj4s <elizavetalenn@mail.ru>
2025-06-19 21:03:42 +0300

    Fix authors page

--- backend/src/routes/authors_router.py
+++ backend/src/routes/authors_router.py
@@ -19,12 +19,10 @@ authors_router = APIRouter(
 async def get_author(author_id: int,
                      db: AsyncSession = Depends(get_db)):
     author = await GeneralDAO.get_item_by_id(db=db, item=models.Author, item_id=int(author_id))
-    CheckHTTP404NotFound(founding_item=author, text="Автор не найден")
     return author
 
 
 @authors_router.get("/authors_list", tags=["authors"])
 async def get_authors(db: AsyncSession = Depends(get_db)):
     authors = await GeneralDAO.get_all_items(db=db, item=models.Author)
-    CheckHTTP404NotFound(founding_item=authors, text="Авторы не найден")
     return authors
